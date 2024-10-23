f539cf885358c063178e460615d225deff85fd56
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2024-10-23 14:16:42 +0530

    coding

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,14 @@ const db = require('../db')
 
 const router = express.Router()
 
+//get index page
 router.get('/index',(req,res,next)=>{
 
     res.locals.filter = null
     res.render('index')
 })
 
+//get home page
 router.get('/',(req,res,next)=>{
 
     res.render('home')
